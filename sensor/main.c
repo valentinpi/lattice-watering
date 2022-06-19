30e3191ef78136c19d5e52b38e5c366d88647b66
Kaan Dönmez <kaad01@zedat.fu-berlin.de>
2022-06-19 20:28:47 +0200

    sensor calibrated

--- sensor/main.c
+++ sensor/main.c
@@ -7,32 +7,21 @@
 #include "shell.h"
 int main(void)
 {
-    puts("Hello World!");
-    adc_t pin0 = ADC_LINE(0);
-    adc_t pin1 = ADC_LINE(1);
-    adc_res_t RES = ADC_RES_10BIT;
-    adc_init(pin0);
-    adc_init(pin1);
-    int moisture0 = 0;
-    int moisture1 = 0;
+    const int DRY_VALUE = 2920;
+    const int WET_VALUE = 1400;
+    adc_t pin = ADC_LINE(1);
+    adc_res_t RES = ADC_RES_12BIT;
+    adc_init(pin);
+    int moisture_value = 0;
+    int moisture_percentage = 0;
 
     while (true){
-	moisture0 = adc_sample(pin0,RES);
-	moisture1 = adc_sample(pin1,RES);
-	printf("Moisture0: %i \n", moisture0);
-	printf("Moisture1: %i \n", moisture1);
+	moisture_value = adc_sample(pin,RES);
+	moisture_percentage = (moisture_value - DRY_VALUE)*100/(WET_VALUE-DRY_VALUE);
+	printf("Moisture: %i \n", moisture_percentage);
 	ztimer_sleep(ZTIMER_MSEC, 100U);
     }
 
-
-//    gpio_mode_t mode = GPIO_IN;
-//    gpio_t pin = GPIO_PIN(0,7);
-//    gpio_init(pin,mode);
-//    while(true){
-//    	int moisture = gpio_read(pin);
-//    	printf("Moisture %i \n",moisture);
-//	ztimer_sleep(ZTIMER_MSEC, 100U);
-//    }
     char line_buf[SHELL_DEFAULT_BUFSIZE];
     shell_run(NULL, line_buf, SHELL_DEFAULT_BUFSIZE);
 
