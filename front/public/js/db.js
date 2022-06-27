1cee4c78ca2441f4a05420dcbf54d88c6e356aee
Jonas.h <herotakka@googlemail.com>
2022-06-27 11:35:52 +0200

    Dynamic adding of plants in frontend, missing some infos Async and await used for most db operations to ensure return data

--- front/public/js/db.js
+++ front/public/js/db.js
@@ -1,38 +1,37 @@
-"use strict"
+//"use strict"
 
+const { response } = require('express');
 var sqlite3 = require('sqlite3');
 
 module.exports = {
-    databaseAccess: function() {
-        let db = new sqlite3.Database('./lattice_watering.db', sqlite3.OPEN_READWRITE, (err) => {
+    databaseAccess: function () {
+        var db_file = './lattice_watering.db';
+        let db = new sqlite3.Database(db_file, sqlite3.OPEN_READWRITE, (err) => {
             if (err && err.code == "SQLITE_CANTOPEN") {
-                console.log('No database: "lattice_watering.db" found');
-                this.createDatabase();
+                console.log('No database: ' + db_file + ' found');
+                this.createDatabase(db_file);
             } else if (err) {
                 console.log("Database Access error " + err);
                 exit(1);
             } else {
-                console.log('Database: "lattice_watering.db" found and connected')
+                console.log('Database: ' + db_file + ' found and connected')
             }
-
-            this.insertQuery(db);
-            this.selectQuery(db, 0);
-            this.selectQuery(db, 1);
         });
+        return db;
     },
 
-    createDatabase: function() {
-        var newdb = new sqlite3.Database('./lattice_watering.db', (err) => {
+    createDatabase: function (db_file) {
+        var newdb = new sqlite3.Database(db_file, (err) => {
             if (err) {
                 console.log("Database creation error " + err);
                 exit(1);
             }
-            console.log('Database: "lattice_watering.db" created')
-            this.createQuery(newdb);
+            console.log('Database: ' + db_file + ' created')
+            this.createTable(newdb);
         });
     },
 
-    createQuery: function(db) {
+    createTable: function(db) {
         db.run(`
         CREATE TABLE plant_nodes (
             id INTEGER PRIMARY KEY AUTOINCREMENT NULL,
@@ -50,12 +49,8 @@ module.exports = {
         });
     },
 
-    insertQuery: function(db) {
-        //placedholder-mania
-        var node_ip = '::1';
-        var plant_name = 'Tomato';
-        var humidity = '42';
-        //date_time creation is asynchonous with nodes, data is not perfect (crashes ...)
+    insertQuery: function(node_ip = '::1', plant_name = 'NULL', humidity = '42') {
+        var db = this.databaseAccess();
         db.run(`
         INSERT INTO plant_nodes (id, node_ip, plant_name, date_time, humidity)
             VALUES (NULL, ?, ?, datetime('now','localtime'), ?);
@@ -68,31 +63,76 @@ module.exports = {
         });
     },
 
-    selectQuery: function(db, query = 0) {
-        if (query == 0) {
+    selectAll: async function () {
+        var db = this.databaseAccess();
+        var data = 0;
+        //Returns all rows from table
+        let myPromise = new Promise(function (resolve) {
             db.all(`SELECT * FROM plant_nodes`, (err, rows) => {
                 if (err) {
                     console.log('Select query error ' + err);
-                    return;
-                };
-                //console.log(rows);
-
-                rows.forEach(row => {
-                    console.log(row.id + "\t" + row.node_ip + "\t" + row.plant_name + "\t" + row.date_time + "\t" + row.humidity);
-                });
+                    resolve(data);
+                } else {
+                    rows.forEach(row => {
+                        console.log(row.id + "\t" + row.node_ip + "\t" + row.plant_name + "\t" + row.date_time + "\t" + row.humidity);
+                    });
+                    resolve(data);
+                }
             });
-        } else if (query == 1) {
-            var data = 0;
+        })
+        var return_data = await myPromise;
+        return return_data;
+    },
+
+    selectCountIps: async function () {
+        var db = this.databaseAccess();
+        var data = 0;
+        //Returns amount of distinct ips in table
+        let myPromise = new Promise(function (resolve) {
             db.all(`SELECT COUNT(DISTINCT node_ip) AS plant_num FROM plant_nodes`, (err, rows) => {
                 if (err) {
-                    console.log('Select query error: ' + err); return;
-                };
-                rows.forEach(row => {
-                    data = row.plant_num;
-                    //console.log(row.plant_num);
-                });
-                console.log('Current Number of plants is: ' + data);
+                    console.log('Select query error: ' + err);
+                    resolve(data);
+                } else {
+                    data = rows[0].plant_num;
+                    resolve(data);
+                }
             });
-        };
+
+        })
+        var return_data = await myPromise;
+        return return_data;
+    },
+
+    selectPlantInfos: async function () {
+        var db = this.databaseAccess();
+        var data = 0;
+        //Returns amount of distinct ips in table
+        let myPromise = new Promise(function (resolve) {
+            db.all(`
+            SELECT
+                id as id,
+                node_ip,
+                plant_name as plant_name,
+                MAX(date_time) as date_time,
+                humidity as humidity
+            FROM plant_nodes
+            GROUP BY node_ip
+            `, (err, rows) => {
+                if (err) {
+                    console.log('Select query error: ' + err);
+                    resolve(data);
+                } else {
+                    rows.forEach(row => {
+                        console.log(row.id + "\t" + row.node_ip + "\t" + row.plant_name + "\t" + row.date_time + "\t" + row.humidity);
+                    });
+                    data = rows[0].plant_num;
+                    resolve(data);
+                }
+            });
+
+        })
+        var return_data = await myPromise;
+        return return_data;
     }
 };
--- front/server.js
+++ front/server.js
@@ -11,6 +11,7 @@ var sqlite3 = require('sqlite3');
 var dtls = require('dtls');
 var db = require('./public/js/db');
 
+
 //Websocket
 var io = require('socket.io')(http);
 
@@ -20,7 +21,6 @@ var URL = require('url');
 var request = require('coap').request;
 var Agent = require('coap').Agent;
 var url;
-
 var debug = false;
 
 /* ---------------- Express Setup ---------------- */
@@ -38,6 +38,21 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+/* ------------------ PlantView ------------------ */
+/*const box = document.createElement("div");
+box.classList.add('box');
+const h3 = document.createElement("h3");
+const img = document.createElement("img");
+img.src = "/img/placeholder_plant.png";
+img.alt = "Plant";
+const text = document.createTextNode = "Humidity: n/a%<br />Temperature: n/a C<br />"
+
+box.appendChild(h3);
+box.appendChild(img);
+box.appendChild(text);
+const element = document.getElementById("scrollmenu");
+element.appendChild(box);*/
+
 /* ------------------- Frontend ------------------ */
 app.get('/plantView', function (req, res) {
     res.render('./plantView.html')
@@ -45,6 +60,14 @@ app.get('/plantView', function (req, res) {
 
 app.get('/', function (req, res) {
     res.render('./index.html');
+});
+
+app.get('/plant_count', async function (req, res) {
+    //let selectAll = await db.selectAll();
+    let plant_count = await db.selectPlantInfos();
+    //console.log("server says: " + plant_count);
+    //res.json(plant_count);
+    res.json('2');
 });
 
 app.post('/pumpToggle', function (req, res) {
@@ -71,16 +94,13 @@ app.post('/pumpToggle', function (req, res) {
             process.exit(0);
         })
     })
-
     db.databaseAccess();
-
     res.status(204).send();
 });
 
 app.listen(3000, () => {
     console.log('listening on port 3000 for frontend requests');
 });
-
 
 /* --------------------- COAP -------------------- */
 var server = coap.createServer();
@@ -98,6 +118,7 @@ server.on('response', (res) => {
     })
 });
 
+
 function parseCoapPayload(data) {
     const char_array = [];
     data.forEach(data_char => {
@@ -107,12 +128,12 @@ function parseCoapPayload(data) {
 }
 
 try {
-    server.listen((null, null, "cert.key", "cert.crt"), () => {
+    server.listen(("localhost", 5684, "cert.key", "cert.crt"), () => {
         console.log('listening on port 5683 for coap requests with dtls');
     });
 } catch (err) {
     console.log("No dtls active: " + err);
-    console.log("First you need to generate a private key and public certificate:\nopenssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout cert.key -out cert.crt -subj '/CN=m4n3dw0lf/O=dtls/C=BR'");
+    console.log("First you need to generate a private key and public certificate");
 
     server.listen(5683, () => {
         console.log('listening on port 5683 for coap requests without dtls');
